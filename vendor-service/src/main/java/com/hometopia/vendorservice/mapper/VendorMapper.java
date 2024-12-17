90ebcd640d9cca0bad5ee2336ae1b0fc6ca8b31c
QuangDuong2903 <quangduongptsc@gmail.com>
2024-12-18 00:41:39 +0700

    adapt code

--- vendor-service/src/main/java/com/hometopia/vendorservice/mapper/VendorMapper.java
+++ vendor-service/src/main/java/com/hometopia/vendorservice/mapper/VendorMapper.java
@@ -3,9 +3,12 @@ package com.hometopia.vendorservice.mapper;
 import com.hometopia.vendorservice.dto.response.GetListVendorResponse;
 import com.hometopia.vendorservice.model.Vendor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface VendorMapper {
     Vendor toVendor(com.hometopia.commons.message.Vendor vendor);
+
+    @Mapping(target = "address", source = "address.line")
     GetListVendorResponse toGetListVendorResponse(Vendor vendor);
 }
